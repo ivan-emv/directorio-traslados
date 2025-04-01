cf8970a08fb78017bc1b16508d95f3372997955f
ivan-emv <ivan.amador@europamundo.com>
2025-04-01 18:05:19 +0200

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -2,15 +2,16 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import streamlit as st
 import json
-import base64
+from ast import literal_eval
 
 @st.cache_resource
 def init_firestore():
-    # Leer el JSON desde secrets y convertirlo a dict
-    key_dict = json.loads(st.secrets["FIREBASE_CREDENTIALS"])
-    cred = credentials.Certificate(key_dict)
+    # Interpretar el contenido de los secrets como diccionario
+    firebase_key = literal_eval(st.secrets["FIREBASE_CREDENTIALS"])
+    cred = credentials.Certificate(firebase_key)
 
     if not firebase_admin._apps:
         firebase_admin.initialize_app(cred)
 
-    return firestore.client()
+    db = firestore.client()
+    return db
